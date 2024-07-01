a4d20620d95655f75a2671dd179b9aabc2e5d0ac
Naizil Francis D Costa <112805712+Naizil-Francis@users.noreply.github.com>
2024-07-01 20:23:57 +0530

    Update 9.py

--- 9.py
+++ 9.py
@@ -1,32 +1,32 @@
 import numpy as np
-import csv
 import pandas as pd
-from pgmpy.models import BayesianModel
+from pgmpy.models import BayesianNetwork
 from pgmpy.estimators import MaximumLikelihoodEstimator
 from pgmpy.inference import VariableElimination
-#read Cleveland Heart Disease data
+import matplotlib.pyplot as plt
+
 heartdisease = pd.read_csv('heart.csv')
-heartdisease = heartdisease.replace('?',np.nan)
-#display the data
+heartdisease = heartdisease.replace('?', np.nan)
+
 print('Few examples from the dataset are given below')
 print(heartdisease.head())
-#Model Bayesian Network
-model=BayesianModel([('age','trestbps'),('age','fbs'),
-('sex','trestbps'),('exang','trestbps'),('trestbps','heartdisease'),('fbs','heartdisease'),('heartdisease','restecg'),
-('heartdisease','thalach'),('heartdisease','chol')])
-#Learning CPDs using Maximum Likelihood Estimators
-print('\n Learning CPD using Maximum likelihood estimators')
+
+model = BayesianNetwork([('age', 'trestbps'), ('age', 'fbs'),
+                         ('sex', 'trestbps'), ('exang', 'trestbps'),
+                         ('trestbps', 'heartdisease'), ('fbs', 'heartdisease'),
+                         ('heartdisease', 'restecg'), ('heartdisease', 'thalach'),
+                         ('chol', 'heartdisease')])
+
+print('\nLearning CPD using Maximum likelihood estimators')
 model.fit(heartdisease, estimator=MaximumLikelihoodEstimator)
-# Inferencing with Bayesian Network
-print('\n Inferencing with Bayesian Network:')
+
+print('\nInferencing with Bayesian Network:')
 HeartDisease_infer = VariableElimination(model)
-#computing the Probability of HeartDisease given Age
-print('\n 1. Probability of HeartDisease given Age=30')
-q=HeartDisease_infer.query(variables=['heartdisease'],evidence
-={'age':28})
-print(q['heartdisease'])
-#computing the Probability of HeartDisease given cholesterol
-print('\n 2. Probability of HeartDisease given cholesterol=100')
-q=HeartDisease_infer.query(variables=['heartdisease'],evidence
-={'chol':100})
-print(q['heartdisease'])
+
+print('\n1. Probability of HeartDisease given Age=30')
+q = HeartDisease_infer.query(variables=['heartdisease'], evidence={'age': 30})
+print(q.values[1])
+
+print('\n2. Probability of HeartDisease given cholesterol=254')
+q = HeartDisease_infer.query(variables=['heartdisease'], evidence={'chol': 254})
+print(q.values[1])
